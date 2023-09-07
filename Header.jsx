219d0ff52f9363992853cf73ef685255061c0373
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-07 16:12:23 +0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+
+function Header(){
+    return(
+        <nav className="deep-purple lighten-2">
+            <div className="nav-wrapper">
+              <a href="#" classNameName="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#">Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
